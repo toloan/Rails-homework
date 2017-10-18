cf93f53b9019fd944535d97e46017f81193ffa60
sotatek-loanhoang <loan.hoang@sotatek.com>
2017-10-18 22:48:29 +0700

    chuong 7

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,4 +1,3 @@
-
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
@@ -6,7 +5,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get root_path
     assert_response :success
-    assert_select "title", "Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
   end
 
   test "should get help" do
--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,3 +1,4 @@
+
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
@@ -10,6 +11,6 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
     get contact_path
-assert_select "title", full_title("Contact")
+    assert_select "title", full_title("Contact")
   end
 end
