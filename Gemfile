c18a326f2cfaac593e46f817c817e5c07f902896
sotatek-loanhoang <loan.hoang@sotatek.com>
2017-10-26 22:18:50 +0700

    ex10

--- Gemfile
+++ Gemfile
@@ -10,6 +10,9 @@ end
 gem 'rails', '~> 5.1.4'
 gem 'bcrypt',         '3.1.11'
 gem 'bootstrap-sass', '3.3.7'
+gem 'faker',          '1.7.3'
+gem 'will_paginate',           '3.1.5'
+gem 'bootstrap-will_paginate', '1.0.0'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
 gem 'jquery-rails', '4.3.1'
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,6 +3,10 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  def current_user?(user)
+  user == current_user
+end
+
   def current_user
     #  @current_user = @current_user || User.find_by(id: session[:user_id]) #find_by return object or nil. nil can be considered as false
     if (user_id = session[:user_id])
@@ -38,4 +42,14 @@ module SessionsHelper
     cookies.delete(:user_id)
     cookies.delete(:remember_token)
   end
+
+  def redirect_back_or(default)
+  redirect_to(session[:forwarding_url] || default)
+  session.delete(:forwarding_url)
+end
+
+# Stores the URL trying to be accessed.
+def store_location
+  session[:forwarding_url] = request.original_url if request.get?
+end
 end
