9a9cbc8f415357982ec6f0d06783aeb8d4bf452a
sotatek-loanhoang <loan.hoang@sotatek.com>
2017-10-29 22:02:43 +0700

    bt ex12

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-    attr_accessor :remember_token, :activation_token
+    attr_accessor :remember_token, :activation_token, :reset_token
     before_create :create_activation_digest
   before_save { self.email = email.downcase }
   validates :name,presence: true, length: {maximum: 50}
@@ -48,5 +48,20 @@ end
   def forget
     update_attribute(:remember_digest, nil)
   end
+
+  # Sets the password reset attributes.
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest,  User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+  end
+
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
+  def password_reset_expired?
+  reset_sent_at < 2.hours.ago
+end
 
 end
