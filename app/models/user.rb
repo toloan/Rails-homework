4b31b5f191eed2880cf0874ecb93f1ce3b41904e
sotatek-loanhoang <loan.hoang@sotatek.com>
2017-10-31 22:31:28 +0700

    following users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,14 @@
 class User < ApplicationRecord
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships, class_name:  "Relationship",
+                                foreign_key: "follower_id",
+                                dependent:   :destroy
+   has_many :following, through: :active_relationships, source: :followed
+   has_many :passive_relationships, class_name:  "Relationship",
+                                 foreign_key: "followed_id",
+                                 dependent:   :destroy
+    has_many :followers, through: :passive_relationships, source: :follower
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     attr_accessor :remember_token, :activation_token, :reset_token
     before_create :create_activation_digest
@@ -8,6 +17,7 @@ class User < ApplicationRecord
   validates :email,presence: true,length: {maximum: 255}, format: {with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+
 
 
       # Creates and assigns the activation token and digest.
@@ -65,7 +75,25 @@ end
     reset_sent_at < 2.hours.ago
   end
   def feed
-    microposts
+    following_ids = "SELECT followed_id FROM relationships
+                     WHERE  follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids})
+                     OR user_id = :user_id", user_id: id)
   end
+
+  # Follows a user.
+def follow(other_user)
+  following << other_user
+end
+
+# Unfollows a user.
+def unfollow(other_user)
+  following.delete(other_user)
+end
+
+# Returns true if the current user is following the other user.
+def following?(other_user)
+  following.include?(other_user)
+end
 
 end
